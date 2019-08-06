ecccd93f1563c57a83480e278f36f8e4b3493ef6
Wise Ilya <johndoe@example.com>
2019-08-06 08:34:11 +0300

    new change

--- coursNew.js
+++ coursNew.js
@@ -33,8 +33,80 @@ function calculateCar() {
 //      console.log(num[i]);
 //  }
 
-let mus = [1,2,3,4,5,6,7,8,9]
+// let mus = [1,2,3,4,5,6,7,8,9]
 
-for(let i = 0; i < mus.length; i++) {
-    console.log(mus[i]);
+// for(let i = 0; i < mus.length; i++) {
+//     console.log(mus[i]);
+// }
+
+
+// let div = document.getElementById('root')
+// let p = document.getElementsByClassName('root')     // старые методы обращение через DOM
+// let h1 = document.getElementsByTagName('h1')
+
+
+// let div = document.querySelector('#nameID')   // позволяет более удобно получить элементы из ID
+// let p = document.querySelectorAll('.p') //  получаем всю колекцию с класса
+// let h1 = document.querySelector('h1') // получаем доступ ко-всем тегам h1
+
+
+
+// function inner() {
+//     let ul = document.querySelector('#root');
+//     ul.innerHTML = '<p>Hello Wise</p>';
+//     return ul;
+// }
+
+// inner()
+
+// const a = document.querySelector('a');
+// const oldHref = a.getAttribute('href')
+// a.setAttribute('href', 'habr.com')
+
+
+// const button = document.querySelector("#button");
+// const p = document.querySelector('#root')
+// const buttonTwo = document.querySelector('#buttontwo');
+    
+//     function nameFunc() {
+//         console.log('Click button');
+//         p.textContent = 'JS';
+//     }
+
+//     function twoFunc() {
+//         console.log('TwoButton');
+//         p.textContent = 'NoJS';
+//     }
+
+// buttonTwo.addEventListener('click', twoFunc);
+// button.addEventListener('click', nameFunc);
+
+// const input = document.querySelector('#inputeText');
+
+//     function inputeT() {
+//         console.log('click');
+//         p.textContent = p.value;
+//     }
+
+//     input.addEventListener('click', inputeT);
+
+let p = document.querySelector('#root');
+let button = document.querySelector('#inputeText');
+let input = document.querySelector('#inputID')
+
+function nameF() {
+    console.log('click');
+    p.textContent = input.value
 }
+
+button.addEventListener('click', nameF);
+
+p.addEventListener('mouseenter', function () {
+    console.log('Mouse up!');
+    p.innerHTML = 'WiseINC';
+})
+                                        // события мыши
+p.addEventListener('mouseleave', function () {
+    console.log('Mouse up!');
+    p.innerHTML = 'NoWISE = )';
+})
