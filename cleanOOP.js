b884619cb127a86cebf9c5908dc970f829f6224a
Wise Ilya <johndoe@example.com>
2019-08-16 21:20:39 +0300

    change new file

--- cleanOOP.js
+++ cleanOOP.js
@@ -1,1 +1,30 @@
-// add 
+
+// let people = [
+//     {name : 'Ilya', age: 20},
+//     {name : 'Ilya', age: 20},
+//     {name : 'Ilya', age: 20},
+//     {name : 'Ilya', age: 20},
+//     {name : 'Ilya', age: 20},
+//     {name : 'Ilya', age: 20},
+// ];
+
+
+// const newPeople = people.map(persone => {
+//     return `${persone.name} (${persone.age})`
+// });
+
+// console.log(newPeople)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
