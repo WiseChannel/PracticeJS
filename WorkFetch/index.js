bf4121c40d1d0bac96379e4bb3ba5c3d81c0803d
Wise Ilya <johndoe@example.com>
2019-08-17 18:49:48 +0300

    test git

--- WorkFetch/index.js
+++ WorkFetch/index.js
@@ -29,3 +29,20 @@ async function load() {
             ul.insertAdjacentHTML('afterbegin', html.join(''))
 
 }
+
+
+
+//test git
+
+// let response = await fetch(url)
+//     let data = await response.json()
+//     console.log(data)
+
+//          let ul = document.querySelector('#list')
+//             let html = data.map(function(item) {
+//                 return '<li>' + item.id + ' ' + item.name + ' (' + item.email +  ')</li>'   // этот код, более кроссбраузерный. Поддерживает больше браузеров
+//             })
+
+//             ul.insertAdjacentHTML('afterbegin', html.join(''))
+
+// }
