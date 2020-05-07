d3704f85522edd9336d07477cb25423d0ea23f23
Ilya <ilyaphotoshop17@gmail.com>
2020-05-07 19:11:52 +0300

    [clear] clear directory

--- coursMinin.js
+++
@@ -1,7 +0,0 @@
-
-let a = document.querySelector('#linkId');
-a.getAttribute('href');
-console.log(a);
-
-a.setAttribute('href', 'google.com');
-a.textContent = 'google.com';
--- promis/feth/async.js
+++
@@ -1,2 +0,0 @@
-import {promise} from '../promise';
-
