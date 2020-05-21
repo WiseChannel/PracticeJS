36ff6decc40af5f9d25deedab5819fe6395497c4
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-05-21 22:46:16 +0300

    [add] new lab

---
+++ LabsDiana/newLabs/index.js
@@ -0,0 +1,46 @@
+
+class figurePerimeterCalculation {
+
+}
+
+class flatFigure {
+    Side(side) {
+        let startingSide = side.weight = 0
+    }
+}
+
+class Triangle {
+    triangle(size) {
+
+    }
+}
+
+class regularTriangle extends flatFigure {
+    constructor(triangle, side) {
+        super();
+        triangle = 1
+        side = new Triangle(triangle)
+    }
+
+    perimeterChange() {
+        // изменение периметра
+    }
+}
+
+class Square extends flatFigure {
+    constructor(triangle, side) {
+        super();
+        triangle = 1
+        side = new Triangle(triangle)
+    }
+
+    perimeterChange() {
+        // изменение периметра
+    }
+}
+
+function main() {
+    const flatFigure = new flatFigure()
+    const Triangle = new Triangle()
+    const regularTriangle = new regularTriangle()
+}
