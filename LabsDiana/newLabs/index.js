3547b2fed71caba08b5023fa180f923c6cc8ec1a
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-05-22 10:59:06 +0300

    [add] create new div elements

--- LabsDiana/newLabs/index.js
+++ LabsDiana/newLabs/index.js
@@ -1,16 +1,31 @@
+const square = document.getElementsByClassName('square')
+const triangleUp = document.getElementsByClassName('triangle-up')
+const container = document.getElementsByClassName('container')
 
+const IncreaseSize = document.querySelector('#IncreaseSize')
+    .addEventListener('click', () => {
+        square.style.backgroundSize = '5'
+        square.style.height = '5px'
+    })
+const CreateNewShape = document.querySelector('#CreateNewShape')
+    //.addEventListener('click', () => {})
+
+// Класс для вычисления периметра фигуры и привязки обработчика к событию "изменение периметра"
 class figurePerimeterCalculation {
-
+    // привязка обработчика к событию
 }
 
+// Базовый класс - полская фигура
 class flatFigure {
     Side(side) {
-        let startingSide = side.weight = 0
+        let startingSide = side.document.style.weight = 0 // начальное значение стороны
+        // изменение периметра
     }
 }
 
-class Triangle {
-    triangle(size) {
+class Triangle{
+    triangle() {
+        const div = document.createElement('div').className = 'square'
 
     }
 }
@@ -44,3 +59,10 @@ function main() {
     const Triangle = new Triangle()
     const regularTriangle = new regularTriangle()
 }
+
+function test() {
+    square.style.weight = '5px'
+    square.style.height = '5px'
+}
+
+console.log(square)
