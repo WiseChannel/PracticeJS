7c2667e083c34a4d80d03a11905d732142c606ae
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-05-22 11:26:37 +0300

    [fix]

--- LabsDiana/newLabs/index.js
+++ LabsDiana/newLabs/index.js
@@ -3,30 +3,41 @@ const triangleUp = document.getElementsByClassName('triangle-up')
 const container = document.getElementsByClassName('container')
 
 const IncreaseSize = document.querySelector('#IncreaseSize')
-    .addEventListener('click', () => {
-        square.style.backgroundSize = '5'
-        square.style.height = '5px'
-    })
 const CreateNewShape = document.querySelector('#CreateNewShape')
-    //.addEventListener('click', () => {})
 
 // Класс для вычисления периметра фигуры и привязки обработчика к событию "изменение периметра"
 class figurePerimeterCalculation {
+    // вычисление периметра фигуры
+    constructor(square, triangleUp) {
+        this.square = square.document.body.style.height
+        this.triangleUp = triangleUp.document.body.style.height
+    }
+
     // привязка обработчика к событию
+    eventHandler() {
+        IncreaseSize.addEventListener('click', () => {
+            const div = document.createElement('div').className = 'square'
+            container.appendChild(div)
+        })
+    }
 }
 
 // Базовый класс - полская фигура
 class flatFigure {
-    Side(side) {
-        let startingSide = side.document.style.weight = 0 // начальное значение стороны
-        // изменение периметра
+    Side() {
+        let startingSide = square.document.style.weight = 0 // начальное значение стороны
+    }
+
+    // изменение периметра
+    perimeterChange() {
+
     }
 }
 
 class Triangle{
     triangle() {
         const div = document.createElement('div').className = 'square'
-
+        container.appendChild(div)
     }
 }
 
@@ -54,15 +65,14 @@ class Square extends flatFigure {
     }
 }
 
-function main() {
-    const flatFigure = new flatFigure()
-    const Triangle = new Triangle()
-    const regularTriangle = new regularTriangle()
-}
+// function main() {
+//     //const flatFigure = new flatFigure()
+//     const Triangle = new Triangle()
+//     Triangle.triangle()
+//     //const regularTriangle = new regularTriangle()
+// }
 
-function test() {
-    square.style.weight = '5px'
-    square.style.height = '5px'
-}
+const triangle = new Triangle()
+triangle.triangle()
 
 console.log(square)
